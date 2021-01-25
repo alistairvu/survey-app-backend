d384d5cf8dabaea155f88cb84d66a9365319dc27
Alistair Vu <alistairvu@gmail.com>
2021-01-25 21:54:22 +0700

    Add process env PORT

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-const port: number = 6960
+const port = process.env.PORT || 6960
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
